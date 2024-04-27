526630401833e40f8a7e77015a6907c78b1246de
rnrabi <rnrabi913@gmail.com>
2024-04-27 22:13:41 +0600

    get data api by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -32,6 +32,13 @@ async function run() {
        
         app.get('/craft' , async(req , res)=>{
             const result = await craftCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.get('/craft/:id' , async(req , res)=>{
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)}
+            const result = await craftCollection.findOne(query);
             res.send(result);
         })
 
